58632b2199159c545ccc028579c18ce07f804add
Fuma <Fuma@141.28.134.205>
2017-06-22 11:28:53 +0200

    A10

--- Eisdiele/eisTest.js
+++ Eisdiele/eisTest.js
@@ -1,1 +1,38 @@
+"use strict";
+console.log("Server starting");
+const Http = require("http");
+const Url = require("url");
+let port = process.env.PORT; // env= information about environment in dem Fall �ber den PORT
+if (port == undefined)
+    port = 8100; //wenn der Port nicht bereits definiert ist soll er 8100 sein.
+let server = Http.createServer(); //ein HTTP-Server wird erstellt
+server.addListener("listening", handleListen); // Listener an Server
+server.addListener("request", handleRequest); // Listener an Antwort
+server.listen(port); // Server soll auf ausgegebenen PORT h�ren
+function handleListen() {
+    console.log("Listening on port: " + port); // Konsole gibt PORT an
+}
+function handleRequest(_request, _response) {
+    // IncomingMessage = wird automatisch von Server erstellt und sendet Parameter an requestListener
+    //                   Methoden: GET, POST
+    // ServerResponse  = wird automatisch von Server erstellt und sendet Parameter an requestListener
+    //                   setHeader, write, end
+    console.log("Request received");
+    console.log(_request.url); //url soll ausgegeben werden
+    let query = Url.parse(_request.url, true).query;
+    // query = vom Typ AssocStringStrin 
+    //         die Url wird geparsed (zu einem JS-Objekt), wird zu einem boolean. Aus dem boolean wird query.
+    console.log(query);
+    let key;
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8"); // als Response wird Header angelegt 
+    for (key in query)
+        _response.write(key + ":" + query[key]);
+    //    _response.setHeader("Access-Control-Allow-Origin", "*");
+    //    _response.setHeader("content-type", "text/html; charset=utf-8"); // als Response wird Header angelegt 
+    //    _response.write("Ich höre Stimmen!"); // in HTML wird "ich h�re Stimmen" geschrieben.
+    //    _response.write("Ich kann auch Stimmen hören :D");
+    //    _response.write(query); // A2.2 Response so angepasst dass query-Daten auch in Response auftauchen.
+    _response.end();
+}
 //# sourceMappingURL=eisTest.js.map
---
+++ Eisdiele/eisTest.ts
@@ -0,0 +1,48 @@
+console.log("Server starting");
+
+import Http = require("http");
+import Url = require("url");
+
+interface AssocStringString { //neuer Typ
+    [key: string]: string;
+}
+
+let port: number = process.env.PORT; // env= information about environment in dem Fall über den PORT
+if (port == undefined)
+    port = 8100; //wenn der Port nicht bereits definiert ist soll er 8100 sein.
+
+let server: Http.Server = Http.createServer(); //ein HTTP-Server wird erstellt
+server.addListener("listening", handleListen); // Listener an Server
+server.addListener("request", handleRequest);  // Listener an Antwort
+server.listen(port); // Server soll auf ausgegebenen PORT hören
+
+function handleListen(): void {
+    console.log("Listening on port: " + port); // Konsole gibt PORT an
+}
+
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    // IncomingMessage = wird automatisch von Server erstellt und sendet Parameter an requestListener
+    //                   Methoden: GET, POST
+    // ServerResponse  = wird automatisch von Server erstellt und sendet Parameter an requestListener
+    //                   setHeader, write, end
+    console.log("Request received");
+
+    console.log(_request.url); //url soll ausgegeben werden
+    let query: AssocStringString = Url.parse(_request.url, true).query;
+    // query = vom Typ AssocStringStrin 
+    //         die Url wird geparsed (zu einem JS-Objekt), wird zu einem boolean. Aus dem boolean wird query.
+    console.log(query);
+    let key: string;
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8"); // als Response wird Header angelegt 
+
+    for (key in query)
+        _response.write(key + ":" + query[key]);
+
+    //    _response.setHeader("Access-Control-Allow-Origin", "*");
+    //    _response.setHeader("content-type", "text/html; charset=utf-8"); // als Response wird Header angelegt 
+    //    _response.write("Ich hÃ¶re Stimmen!"); // in HTML wird "ich höre Stimmen" geschrieben.
+    //    _response.write("Ich kann auch Stimmen hÃ¶ren :D");
+    //    _response.write(query); // A2.2 Response so angepasst dass query-Daten auch in Response auftauchen.
+    _response.end();
+}
