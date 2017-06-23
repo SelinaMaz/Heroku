0bdf449901704f2646d28ba493de41169aad521f
Fuma <Fuma@Alex.hfu-open.fu.hs-furtwangen.de>
2017-06-23 11:17:09 +0200

    10

--- NodeTest.js
+++ NodeTest.js
@@ -30,7 +30,7 @@ function handleRequest(_request, _response) {
         _response.write(key + ":" + query[key]);
     //    _response.setHeader("Access-Control-Allow-Origin", "*");
     //    _response.setHeader("content-type", "text/html; charset=utf-8"); // als Response wird Header angelegt 
-    //    _response.write("Ich höre Stimmen!"); // in HTML wird "ich h�re Stimmen" geschrieben.
+    _response.write("Ich höre Stimmen!"); // in HTML wird "ich h�re Stimmen" geschrieben.
     //    _response.write("Ich kann auch Stimmen hören :D");
     //    _response.write(query); // A2.2 Response so angepasst dass query-Daten auch in Response auftauchen.
     _response.end();
--- NodeTest.ts
+++ NodeTest.ts
@@ -41,7 +41,7 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
 
     //    _response.setHeader("Access-Control-Allow-Origin", "*");
     //    _response.setHeader("content-type", "text/html; charset=utf-8"); // als Response wird Header angelegt 
-    //    _response.write("Ich hÃ¶re Stimmen!"); // in HTML wird "ich höre Stimmen" geschrieben.
+      _response.write("Ich hÃ¶re Stimmen!"); // in HTML wird "ich höre Stimmen" geschrieben.
     //    _response.write("Ich kann auch Stimmen hÃ¶ren :D");
     //    _response.write(query); // A2.2 Response so angepasst dass query-Daten auch in Response auftauchen.
     _response.end();
